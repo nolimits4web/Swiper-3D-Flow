37b6d041e657811e2bb27cdcee804c2df7b0068e
Stéphane Bachelier <stephane.bachelier@gmail.com>
2014-02-05 11:10:49 +0100

    Add umd build

--- Gruntfile.js
+++ Gruntfile.js
@@ -17,19 +17,19 @@ module.exports = function (grunt) {
         // Metadata.
         pkg: grunt.file.readJSON('bower.json'),
         banner: '/*\n' +
-          ' * Swiper 3D Flow <%= pkg.version %>\n' +
-          ' * <%= pkg.description %>\n' +
-          ' *\n' +
-          ' * <%= pkg.homepage %>\n' +
-          ' *\n' +
-          ' * Copyright 2010-<%= grunt.template.today("yyyy") %>, <%= pkg.author %>\n' +
-          ' * The iDangero.us\n' +
-          ' * http://www.idangero.us/\n' +
-          ' *\n' +
-          ' * Licensed under <%= pkg.license.join(" & ") %>\n' +
-          ' *\n' +
-          ' * Released on: <%= grunt.template.today("mmmm d, yyyy") %>\n' +
-          '*/\n',
+        ' * Swiper 3D Flow <%= pkg.version %>\n' +
+        ' * <%= pkg.description %>\n' +
+        ' *\n' +
+        ' * <%= pkg.homepage %>\n' +
+        ' *\n' +
+        ' * Copyright 2010-<%= grunt.template.today("yyyy") %>, <%= pkg.author %>\n' +
+        ' * The iDangero.us\n' +
+        ' * http://www.idangero.us/\n' +
+        ' *\n' +
+        ' * Licensed under <%= pkg.license.join(" & ") %>\n' +
+        ' *\n' +
+        ' * Released on: <%= grunt.template.today("mmmm d, yyyy") %>\n' +
+        '*/\n',
         // Task configuration.
         concat: {
             options: {
@@ -37,8 +37,12 @@ module.exports = function (grunt) {
                 stripBanners: true
             },
             js: {
-                src: ['lib/<%= swiper.filename %>.js'],
+                src: ['dist/<%= swiper.filename %>.js'],
                 dest: 'dist/<%= swiper.filename %>.js'
+            },
+            umd: {
+                src: ['<%= umd.lib.dest %>'],
+                dest: 'dist/<%= swiper.filename %>.amd.js'
             },
             css: {
                 src: ['lib/<%= swiper.filename %>.css'],
@@ -65,9 +69,13 @@ module.exports = function (grunt) {
             options: {
                 banner: '<%= banner %>'
             },
-            dist: {
+            lib: {
                 src: ['dist/<%= swiper.filename %>.js'],
                 dest: 'dist/<%= swiper.filename %>.min.js',
+            },
+            umd: {
+                src: ['dist/<%= swiper.filename %>.amd.js'],
+                dest: 'dist/<%= swiper.filename %>.amd.min.js',
             }
         },
         jshint: {
@@ -79,8 +87,36 @@ module.exports = function (grunt) {
                 src: ['Gruntfile.js']
             },
             lib: {
-                src: ['lib/*.js']
+                src: ['lib/<%= swiper.filename %>.js']
             },
+        },
+        umd: {
+            lib: {
+                src: '<%= jshint.lib.src %>',
+                dest: 'dist/<%= swiper.filename %>.umd.js',
+                amdModuleId: 'swiper-3d-flow',
+                objectToExport: 'Swiper',
+                indent: '    ',
+                deps: {
+                    'default': ['swiper'],
+                    'amd': ['swiper'],
+                    'cjs': ['swiper'],
+                    'global': ['Swiper']
+                }
+            }
+        },
+        wrap: {
+            js: {
+                src: ['<%= jshint.lib.src %>'],
+                dest: 'dist/<%= swiper.filename %>.js',
+                options: {
+                    wrapper: [
+                        '(function (Swiper) {\n',
+                        '\n})(Swiper);'
+                    ],
+                    indent: '    '
+                }
+            }
         },
         watch: {
             gruntfile: {
@@ -95,12 +131,23 @@ module.exports = function (grunt) {
     });
 
     // Default task.
-    this.registerTask('default', 'build');
+    this.registerTask('default', ['jshint', 'build']);
 
     // Build a new version of the library
     this.registerTask('build', 'Builds a distributable version of <%= pkg.name %>', [
-        'jshint',
-        'concat:js',
+        'wrap:js',
+        'concat:js'
+    ]);
+
+    this.registerTask('build-umd', 'Builds a umd compatible distributable version of <%= pkg.name %>', [
+        'umd:lib',
+        'concat:umd',
+    ]);
+
+    this.registerTask('dist', 'Build dist of <%= pkg.name %>', [
+        'jshint:lib',
+        'build',
+        'build-umd',
         'uglify',
         'concat:css'
     ]);
